7b22d8a2a937c5feda1b45bd8f33557da87f593c
unknown <neil@rehabstudio.com>
2015-08-11 00:27:48 +0100

    Added first AlgoViz handler

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 var Pumper = require('pumper'),
-    Test = require('./channels/test');
+    AlgoViz = require('./channels/algoviz');
 
 var TRACK = 'https://dl.dropboxusercontent.com/u/42386473/cp/Hotline%20Miami%202%20OST%20-%20Sexualizer%20%28Perturbator%29.mp3';
 
-//Pumper.start(TRACK, true);
-Pumper.start('mic');
+Pumper.start(TRACK, true);
+Pumper.globalSpikeTolerance = 15;
+//Pumper.start('mic');
 
-Test.init();
+AlgoViz.init();
 
 console.log(Pumper);
