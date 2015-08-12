960ccd1b26b15cd3d40eb8fbc1a63c1fa8ebd40d
Neil McCallion <neil@rehabstudio.com>
2015-08-12 15:42:05 +0100

    Added jquery, OSD and keyboard shortcuts
    Former-commit-id: eb4cd7798e18159af0f9f410ad2332806d056b4d

--- main.js
+++ main.js
@@ -5,7 +5,9 @@ var CHANNELS = {
         author: 'Neil'
     }
 }
+var CHANNEL_IDS = Object.keys(CHANNELS);
 
+var OSD_HANG_TIME = 2000;
 
 var currentChannel = null;
 
@@ -20,17 +22,46 @@ var els = {
     mainframe: getEl('mainframe')
 };
 
+var _ti;
 function changeChannel(id) {
     console.log('change channel', id);
     var src = (id === null) ? '' : './channels/' + id;
     els.mainframe.setAttribute('src', src);
-    cuurentChannel = id;
+    currentChannel = id;
+
+    var name, author;
+
+    if(currentChannel) {
+        name = id;
+        author = CHANNELS[currentChannel].author;
+    } else {
+        name = 'RHB';
+        author = '...';
+    }
+
+    clearTimeout(_ti);
+    els.channelName.textContent = name;
+    els.channelAuthor.textContent = author;
+    els.osd.classList.add('visible');
+    setTimeout(function() {
+        els.osd.classList.remove('visible');
+    }, OSD_HANG_TIME);
 }
 
+$(document).on('keypress', function(e) {
+    var k = e.which || e.keyCode,
+        nk = k - 48;
+        console.log('keypress', k, nk);
 
-setTimeout(function() {
+    if (nk >= 0 && nk <= CHANNEL_IDS.length) {
+        var cid = (nk) ? CHANNEL_IDS[nk - 1] : null;
+        changeChannel(cid);
+    }
+});
+
+/*setTimeout(function() {
     changeChannel('algoviz');
-}, 4000);
+}, 4000);*/
 
 
 
