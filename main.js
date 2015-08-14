86c94fc674f5460a4b82b334cdd384e230d297c7
Neil McCallion <neil@rehabstudio.com>
2015-08-14 10:19:51 +0100

    Cleanup and timings
    Former-commit-id: 3dbb4cbdf42424eb086a197c0eb6ecbe281a514f

--- main.js
+++ main.js
@@ -4,35 +4,30 @@
 
 console.log('loaded');
 
+var DEFAULT_CHANNEL_DURATION = 60 * 2;
+
 var CHANNELS = {
     'algoviz': {
-        author: 'Neil',
-        duration: 10
+        author: 'Neil'
     },
     'beatprocessing': {
-        author: 'Mick',
-        duration: 10
+        author: 'Mick'
     },
     'mosaic': {
-        author: 'Grieve',
-        duration: 20
+        author: 'Grieve'
     },
     'dirtygif': {
-        author: 'Neil',
-        duration: 10
+        author: 'Neil'
     },
     'reactivelogo': {
-        author: 'Pedro',
-        duration: 20
+        author: 'Pedro'
     },
     'timetraveller': {
-        author: 'Grieve',
-        duration: 20
+        author: 'Grieve'
     }
 };
 
 var CHANNEL_IDS = Object.keys(CHANNELS),
-    DEFAULT_CHANNEL_DURATION = 10,
     OSD_HANG_TIME = 2000;
 
 var currentChannel = null,
