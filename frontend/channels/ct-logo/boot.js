e0241bd56e13702dbf7f2a98e09c15a1eee8424f
Daniel Porter <dpreid@gmail.com>
2019-03-11 16:48:53 +0000

    ct-initial import
    Former-commit-id: 506fe8db278a927b1bec23e75ec9440c88a8837e

---
+++ frontend/channels/ct-logo/boot.js
@@ -0,0 +1,16 @@
+var Pumper = require('pumper'),
+    BeatProcessing = require('./index');
+
+var TRACK = '../../audio/pacemaker.mp3';
+
+//Pumper.start(TRACK, true);
+Pumper.start('mic');
+Pumper.globalSpikeTolerance = 14;
+
+
+
+var f = new FontFace('Patua One', 'url(../../assets/fonts/PatuaOne-Regular.ttf)', {});
+f.load().then(function (loadedFace) {
+    document.fonts.add(loadedFace);
+    BeatProcessing.init();
+});
