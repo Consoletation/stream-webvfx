f5661c833ef6fea5bddbfe0d855bf23380b6ead1
Daniel Porter <dpreid@gmail.com>
2020-09-19 17:39:26 +0100

    Move to glsify-loader for shaders

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -32,8 +32,12 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.glsl$/,
-                loader: 'webpack-glsl'
+                test: /\.(glsl|vs|fs|vert|frag)$/,
+                exclude: /node_modules/,
+                use: [
+                    'raw-loader',
+                    'glslify-loader'
+                ]
             }
         ]
     }
