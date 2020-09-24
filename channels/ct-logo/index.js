9f877b18a17c4186f9fa776574c540642de888b1
Daniel Porter <dpreid@gmail.com>
2020-09-24 17:24:47 +0100

    Tween configuration

--- channels/ct-logo/index.js
+++ channels/ct-logo/index.js
@@ -83,20 +83,37 @@ const animConfig = new function() {
                 global: { x: 1, y: 0.3, z: 0.09 },
             },
         },
-    }
+        tween: {
+            base: {
+                time: 1200,
+                easing: TWEEN.Easing.Quintic.InOut,
+            },
+        },
+    };
     this.profiles.main.positions = {
         base: {
             logo: { y: [0, 8, 0, 0], z: [0, 0, 0, 0] },
             headings: { y: -900, z: 0 },
             camera: { x: -43, y: -90, z: 1000 },
         },
-    }
+    };
+    this.profiles.main.positions.tween = this.profiles.main.multipliers.tween;
     this.profiles.main.directions = {
         base: {
             camera: { x: 0, y: 0, z: 0 },
         },
         transition: {
             camera: { x: 0.18, y: 0, z: 0 },
+        },
+        tween: {
+            base: {
+                time: 1200,
+                easing: TWEEN.Easing.Sinusoidal.Out,
+            },
+            transition: {
+                time: 600,
+                easing: TWEEN.Easing.Sinusoidal.In,
+            },
         },
     };
 
@@ -117,20 +134,32 @@ const animConfig = new function() {
                 global: { x: 0, y: 0.2, z: 0 },
             },
         },
-    }
+        tween: {
+            base: {
+                time: 300,
+                easing: TWEEN.Easing.Quintic.InOut,
+            },
+        },
+    };
     this.profiles.low.positions = {
         base: {
             logo: { y: [0, 0, 0, 0], z: [0, 0, 0, 0] },
             headings: { y: -1000, z: 0 },
             camera: { x: -43, y: 100, z: 720 },
         },
-    }
+    };
+    this.profiles.low.positions.tween = this.profiles.low.multipliers.tween;
     this.profiles.low.directions = {
         base: {
             camera: { x: 0, y: 0, z: 0 },
         },
+        tween: {
+            base: {
+                time: 300,
+                easing: TWEEN.Easing.Sinusoidal.InOut,
+            },
+        },
     };
-    this.profiles.low.directions.transition = this.profiles.low.directions.base; // Compat
 
     // Functions
     this.loadProfile = function(profile) {
@@ -141,39 +170,68 @@ const animConfig = new function() {
         };
         return this.current;
     }
-}
+    this.setupTweens = function() {
+        this.transition = {};
+        // Set up profiles
+        this.transition.main = {};
+        this.transition.main.tweens = [];
+        // These won't work
+        this.transition.main.start = function() {
+            this.transition.main.tweens.forEach(function(tween) {
+                tween.start();
+            });
+        };
+        this.transition.low = {};
+        this.transition.low.tweens = [];
+        // These won't work
+        this.transition.low.start = function() {
+            this.transition.low.tweens.forEach(function(tween) {
+                tween.start();
+            });
+        };
+        // Set up tweens
+        let current = this.current;
+        let main = this.profiles.main;
+        let low = this.profiles.low;
+        const mainMultipliers = new TWEEN.Tween(current.multipliers)
+            .to(main.multipliers.base, main.multipliers.tween.base.time)
+            .easing(main.multipliers.tween.base.easing);
+        const mainPositions = new TWEEN.Tween(current.positions)
+            .to(main.positions.base, main.positions.tween.base.time)
+            .easing(main.positions.tween.base.easing);
+        const mainDirections = new TWEEN.Tween(current.directions)
+            .to(main.directions.transition, main.directions.tween.transition.time)
+            .easing(main.directions.tween.transition.easing);
+        const mainDirections2 = new TWEEN.Tween(current.directions)
+            .to(main.directions.base, main.directions.tween.base.time)
+            .easing(main.directions.tween.base.easing);
+        mainDirections.chain(mainDirections2);
+        this.transition.main.tweens.push(
+            mainMultipliers,
+            mainPositions,
+            mainDirections
+        );
+        const lowMultipliers = new TWEEN.Tween(current.multipliers)
+            .to(low.multipliers.base, low.multipliers.tween.base.time)
+            .easing(low.multipliers.tween.base.easing);
+        const lowPositions = new TWEEN.Tween(current.positions)
+            .to(low.positions.base, low.positions.tween.base.time)
+            .easing(low.positions.tween.base.easing);
+        const lowDirections = new TWEEN.Tween(current.directions)
+            .to(low.directions.transition, low.directions.tween.base.time)
+            .easing(low.directions.tween.base.easing);
+        this.transition.low.tweens.push(
+            lowMultipliers,
+            lowPositions,
+            lowDirections
+        );
+    };
+};
 
 // Current animation presets
 // Updated via tweens and applied within update()
 animConfig.loadProfile('main');
-
-// Set up tweens
-const newTweenMultipliersLow = new TWEEN.Tween(animConfig.current.multipliers)
-    .to(animConfig.profiles.low.multipliers.base, 300)
-    .easing(TWEEN.Easing.Quintic.InOut);
-const newTweenMultipliersMain = new TWEEN.Tween(animConfig.current.multipliers)
-    .to(animConfig.profiles.main.multipliers.base, 1200)
-    .easing(TWEEN.Easing.Quintic.InOut);
-const newTweenPositionsLow = new TWEEN.Tween(animConfig.current.positions)
-    .to(animConfig.profiles.low.positions.base, 300)
-    .easing(TWEEN.Easing.Quintic.InOut);
-const newTweenPositionsMain = new TWEEN.Tween(animConfig.current.positions)
-    .to(animConfig.profiles.main.positions.base, 1200)
-    .easing(TWEEN.Easing.Quintic.InOut);
-const newTweenDirectionsLow = new TWEEN.Tween(animConfig.current.directions)
-    .to(animConfig.profiles.low.directions.transition, 150)
-    .easing(TWEEN.Easing.Sinusoidal.In);
-const newTweenDirectionsLow2 = new TWEEN.Tween(animConfig.current.directions)
-    .to(animConfig.profiles.low.directions.base, 150)
-    .easing(TWEEN.Easing.Sinusoidal.Out);
-newTweenDirectionsLow.chain(newTweenDirectionsLow2);
-const newTweenDirectionsMain = new TWEEN.Tween(animConfig.current.directions)
-    .to(animConfig.profiles.main.directions.transition, 600)
-    .easing(TWEEN.Easing.Sinusoidal.In);
-const newTweenDirectionsMain2 = new TWEEN.Tween(animConfig.current.directions)
-    .to(animConfig.profiles.main.directions.base, 1200)
-    .easing(TWEEN.Easing.Sinusoidal.Out);
-newTweenDirectionsMain.chain(newTweenDirectionsMain2);
+animConfig.setupTweens(); // Set up tweens
 
 function init() {
 
@@ -552,14 +610,16 @@ function update() {
         mainViewUpdate = mainView;
         if (mainViewUpdate) {
             console.log('Tweening to main');
-            newTweenMultipliersMain.start();
-            newTweenPositionsMain.start();
-            newTweenDirectionsMain.start();
+            //animConfig.transition.main.start();
+            animConfig.transition.main.tweens.forEach(function(tween) {
+                tween.start();
+            });
         } else {
             console.log('Tweening to low');
-            newTweenMultipliersLow.start();
-            newTweenPositionsLow.start();
-            newTweenDirectionsLow.start();
+            //animConfig.transition.low.start();
+            animConfig.transition.low.tweens.forEach(function(tween) {
+                tween.start();
+            });
         }
     }
 
