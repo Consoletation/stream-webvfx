dde84a392adc888be8ce895a2453408aa79fc7e7
Daniel Porter <dpreid@gmail.com>
2020-09-25 13:37:21 +0100

    Move preset transition and tween data to arrays
    With arrays we can set up just one, or multiple transition stages.
    Ordering has been swapped for readability, we go from start to end for
    each preset.

--- channels/ct-logo/AnimationConfig.js
+++ channels/ct-logo/AnimationConfig.js
@@ -1,5 +1,11 @@
 import TWEEN from '@tweenjs/tween.js';
 
+if (!Array.prototype.last) {
+    Array.prototype.last = function() {
+        return this[this.length - 1];
+    };
+};
+
 function jCopy(object) {
     return JSON.parse(JSON.stringify(object));
 };
@@ -15,195 +21,213 @@ const profiles = {
     lowsplit: {},
 };
 profiles.main.multipliers = {
-    base: {
-        letters: {
-            high: { y: [1, 0.1, 1.95, 1.5], z: [0, 0, 0, 0] },
-            mid: { y: [0.5, 0.1, 0.8, 0.4], z: [0, 0, 0, 0] },
-            low: { y: [0, 0, 0, 0], z: [1, 1, 1, 1] },
-            global: { y: [0, 0, 0, 0], z: [0.5, 0.5, 0.5, 0.5] },
+    data: [
+        {
+            letters: {
+                high: { y: [1, 0.1, 1.95, 1.5], z: [0, 0, 0, 0] },
+                mid: { y: [0.5, 0.1, 0.8, 0.4], z: [0, 0, 0, 0] },
+                low: { y: [0, 0, 0, 0], z: [1, 1, 1, 1] },
+                global: { y: [0, 0, 0, 0], z: [0.5, 0.5, 0.5, 0.5] },
+            },
+            headings: {
+                global: { y: 0.3, z: 0 },
+            },
+            camera: {
+                global: { x: 1, y: 0.3, z: 0.09 },
+            },
         },
-        headings: {
-            global: { y: 0.3, z: 0 },
-        },
-        camera: {
-            global: { x: 1, y: 0.3, z: 0.09 },
-        },
-    },
-    tween: {
-        base: {
+    ],
+    tween: [
+        {
             time: 1200,
             easing: TWEEN.Easing.Quintic.InOut,
         },
-    },
+    ],
 };
 profiles.main.positions = {
-    base: {
-        logo: [
-            { x: 0, y: 0 },
-            { x: 0, y: 0 },
-        ],
-        letters: { y: [0, 8, 0, 0], z: [0, 0, 0, 0] },
-        image: { x: 0, y: 0 },
-        headings: { y: -900, z: 0 },
-        camera: { x: -43, y: -90, z: 1000 },
-    },
-    transition: {
-        logo: [
-            { x: 0, y: 68 },
-            { x: 0, y: -68 },
-        ],
-        letters: { y: [0, 8, 0, 0], z: [0, 0, 0, 0] },
-        image: { x: 0, y: -1600 },
-        headings: { y: -900, z: 0 },
-        camera: { x: -43, y: -90, z: 1000 },
-    },
-    tween: {
-        base: {
-            time: 600,
-            easing: TWEEN.Easing.Sinusoidal.InOut,
+    data: [
+        {
+            logo: [
+                { x: 0, y: 68 },
+                { x: 0, y: -68 },
+            ],
+            letters: { y: [0, 8, 0, 0], z: [0, 0, 0, 0] },
+            image: { x: 0, y: -1600 },
+            headings: { y: -900, z: 0 },
+            camera: { x: -43, y: -90, z: 1000 },
         },
-        transition: {
+        {
+            logo: [
+                { x: 0, y: 0 },
+                { x: 0, y: 0 },
+            ],
+            letters: { y: [0, 8, 0, 0], z: [0, 0, 0, 0] },
+            image: { x: 0, y: 0 },
+            headings: { y: -900, z: 0 },
+            camera: { x: -43, y: -90, z: 1000 },
+        },
+    ],
+    tween: [
+        {
             time: 300,
             easing: TWEEN.Easing.Sinusoidal.InOut,
         },
-    },
+        {
+            time: 600,
+            easing: TWEEN.Easing.Sinusoidal.InOut,
+        },
+    ],
 };
 profiles.main.directions = {
-    base: {
-        camera: { x: 0, y: 0, z: 0 },
-    },
-    transition: {
-        camera: { x: 0.18, y: 0, z: 0 },
-    },
-    tween: {
-        base: {
-            time: 1200,
-            easing: TWEEN.Easing.Sinusoidal.Out,
+    data: [
+        {
+            camera: { x: 0.18, y: 0, z: 0 },
         },
-        transition: {
+        {
+            camera: { x: 0, y: 0, z: 0 },
+        },
+    ],
+    tween: [
+        {
             time: 600,
             easing: TWEEN.Easing.Sinusoidal.In,
         },
-    },
+        {
+            time: 1200,
+            easing: TWEEN.Easing.Sinusoidal.Out,
+        },
+    ],
 };
 profiles.main.references = {
-    base: {
-        image: {
-            current: 0,
-            tracker: [1, 5],
+    data: [
+        {
+            image: {
+                current: 0,
+                tracker: [1, 5],
+            },
         },
-    },
-    tween: {
-        base: {
+    ],
+    tween: [
+        {
             time: 1200,
             easing: Round,
         },
-    },
+    ],
 };
 profiles.low.multipliers = {
-    base: {
-        letters: {
-            high: { y: [0.6, 0.6, 0.6, 0.6], z: [0, 0, 0, 0] },
-            mid: { y: [0.2, 0.2, 0.2, 0.2], z: [0, 0, 0, 0] },
-            low: { y: [0, 0, 0, 0], z: [0, 0, 0, 0] },
-            global: { y: [0, 0, 0, 0], z: [0, 0, 0, 0] },
+    data: [
+        {
+            letters: {
+                high: { y: [0.6, 0.6, 0.6, 0.6], z: [0, 0, 0, 0] },
+                mid: { y: [0.2, 0.2, 0.2, 0.2], z: [0, 0, 0, 0] },
+                low: { y: [0, 0, 0, 0], z: [0, 0, 0, 0] },
+                global: { y: [0, 0, 0, 0], z: [0, 0, 0, 0] },
+            },
+            headings: {
+                global: { y: 0, z: 0 },
+            },
+            camera: {
+                global: { x: 0, y: 0.2, z: 0 },
+            },
         },
-        headings: {
-            global: { y: 0, z: 0 },
-        },
-        camera: {
-            global: { x: 0, y: 0.2, z: 0 },
-        },
-    },
-    tween: {
-        base: {
+    ],
+    tween: [
+        {
             time: 300,
             easing: TWEEN.Easing.Quintic.InOut,
         },
-    },
+    ],
 };
 profiles.low.positions = {
-    base: {
-        logo: [
-            { x: 0, y: 0 },
-            { x: 0, y: 0 },
-        ],
-        letters: { y: [0, 0, 0, 0], z: [0, 0, 0, 0] },
-        image: { x: 0, y: 0 },
-        headings: { y: -1000, z: 0 },
-        camera: { x: -43, y: 100, z: 720 },
-    },
+    data: [
+        {
+            logo: [
+                { x: 0, y: 0 },
+                { x: 0, y: 0 },
+            ],
+            letters: { y: [0, 0, 0, 0], z: [0, 0, 0, 0] },
+            image: { x: 0, y: 0 },
+            headings: { y: -1000, z: 0 },
+            camera: { x: -43, y: 100, z: 720 },
+        },
+    ],
 };
 profiles.low.positions.tween = profiles.low.multipliers.tween;
 profiles.low.directions = {
-    base: {
-        camera: { x: 0, y: 0, z: 0 },
-    },
-    tween: {
-        base: {
+    data: [
+        {
+            camera: { x: 0, y: 0, z: 0 },
+        },
+    ],
+    tween: [
+        {
             time: 300,
             easing: TWEEN.Easing.Sinusoidal.InOut,
         },
-    },
+    ],
 };
 profiles.low.references = profiles.main.references;
 profiles.lowsplit.multipliers = profiles.low.multipliers;
 profiles.lowsplit.positions = {
-    base: {
-        logo: [
-            { x: 259, y: 68 },
-            { x: -258, y: -68 },
-        ],
-        letters: { y: [0, 0, 0, 0], z: [0, 0, 0, 0] },
-        image: { x: -88, y: 118 },
-        headings: { y: -1000, z: 0 },
-        camera: { x: -43, y: 100, z: 720 },
-    },
-    transition: {
-        logo: [
-            { x: 0, y: 68 },
-            { x: 0, y: -68 },
-        ],
-        letters: { y: [0, 0, 0, 0], z: [0, 0, 0, 0] },
-        image: { x: 0, y: -400 },
-        headings: { y: -1000, z: 0 },
-        camera: { x: -43, y: 100, z: 720 },
-    },
-    tween: {
-        base: {
+    data: [
+        {
+            logo: [
+                { x: 0, y: 68 },
+                { x: 0, y: -68 },
+            ],
+            letters: { y: [0, 0, 0, 0], z: [0, 0, 0, 0] },
+            image: { x: 0, y: -400 },
+            headings: { y: -1000, z: 0 },
+            camera: { x: -43, y: 100, z: 720 },
+        },
+        {
+            logo: [
+                { x: 259, y: 68 },
+                { x: -258, y: -68 },
+            ],
+            letters: { y: [0, 0, 0, 0], z: [0, 0, 0, 0] },
+            image: { x: -88, y: 118 },
+            headings: { y: -1000, z: 0 },
+            camera: { x: -43, y: 100, z: 720 },
+        },
+    ],
+    tween: [
+        {
+            time: 600,
+            easing: TWEEN.Easing.Sinusoidal.InOut,
+        },
+        {
             time: 300,
             easing: TWEEN.Easing.Sinusoidal.InOut,
         },
-        transition: {
-            time: 600,
-            easing: TWEEN.Easing.Sinusoidal.InOut,
-        },
-    },
+    ],
 };
 profiles.lowsplit.directions = profiles.low.directions;
 profiles.lowsplit.references = {
-    base: {
-        image: {
-            current: 1,
-            tracker: [1, 0],
+    data: [
+        {
+            image: {
+                current: 0,
+                tracker: [1, 5],
+            },
         },
-    },
-    transition: {
-        image: {
-            current: 0,
-            tracker: [1, 5],
+        {
+            image: {
+                current: 1,
+                tracker: [1, 0],
+            },
         },
-    },
-    tween: {
-        base: {
-            time: 1,
-            easing: Round,
-        },
-        transition: {
+    ],
+    tween: [
+        {
             time: 600,
             easing: Round,
         },
-    },
+        {
+            time: 1,
+            easing: Round,
+        },
+    ],
 };
 
 class AnimationConfig {
@@ -211,10 +235,10 @@ class AnimationConfig {
     // Set up an active configuration based on 'profile'
     // Tween data configured for all available profiles
     constructor(profile) {
-        this.multipliers = jCopy(profiles[profile].multipliers.base);
-        this.positions = jCopy(profiles[profile].positions.base);
-        this.directions = jCopy(profiles[profile].directions.base);
-        this.references = jCopy(profiles[profile].references.base);
+        this.multipliers = jCopy(profiles[profile].multipliers.data.last());
+        this.positions = jCopy(profiles[profile].positions.data.last());
+        this.directions = jCopy(profiles[profile].directions.data.last());
+        this.references = jCopy(profiles[profile].references.data.last());
         this._tweens = {};
         // set up tweens
         for (const [profile, config] of Object.entries(profiles)) {
@@ -227,14 +251,13 @@ class AnimationConfig {
     // If we have an intermediate (transition) we set up a chain
     setupTween(config) {
         const tweens = [];
-        for (const [preset, data] of Object.entries(config)) {
-            let tween = new TWEEN.Tween(this[preset])
-                .to(data.base, data.tween.base.time)
-                .easing(data.tween.base.easing);
-            if ('transition' in data.tween) {
-                let tween2 = new TWEEN.Tween(this[preset])
-                    .to(data.transition, data.tween.transition.time)
-                    .easing(data.tween.transition.easing);
+        for (const [preset, settings] of Object.entries(config)) {
+            var tween = new TWEEN.Tween(); // Null end
+            // Go from end to start for tween chain
+            for (let i = settings.data.length - 1; i >= 0; i--) {
+                var tween2 = new TWEEN.Tween(this[preset])
+                    .to(settings.data[i], settings.tween[i].time)
+                    .easing(settings.tween[i].easing);
                 tween2.chain(tween);
                 tween = tween2;
             }
